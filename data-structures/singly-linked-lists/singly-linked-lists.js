af076f22b5438d43f4a3e5b49f5077bb10f94c12
Nelson_K <kamaunelson14@gmail.com>
2024-09-25 17:36:31 +0300

    add shift and unshift

--- data-structures/singly-linked-lists/singly-linked-lists.js
+++ data-structures/singly-linked-lists/singly-linked-lists.js
@@ -12,7 +12,7 @@ class SinglyLinkedList{
         this.length = 0;
     }
 
-    // push - adds a value to the end of the list
+    // push - adds a new node to the end of the list
     // Create a new node using the value passed to the function
     // If there is not head property on the list, set the thead and tail to be the newly created node
     // Otherwise, set the next property on the tail to be the new node and set the tail property on the list to be the newly created node
@@ -30,7 +30,7 @@ class SinglyLinkedList{
         this.length++;
     }
 
-    // pop - removes the value at the end of the list
+    // pop - removes the last node from the list
     // If there are no nodes in the list, return undefined
     // Loop through the list until you reach the tail
     // Set the next property of the second last node to be null
@@ -57,8 +57,45 @@ class SinglyLinkedList{
 
         return current;
     }
+
+    // shift - removes the first node from the list
+    // If there are no nodes in the list, return undefined
+    // Store the current head property in a variable
+    // Set the head property to be the current head's next property
+    // Decrement the length by 1
+    // Return the value of the node removed
+    shift(){
+        if(!this.head) return undefined;
+
+        const currentHead = this.head;
+        this.head = this.head.next;
+        this.length--;
+        if(this.length === 0){
+            this.tail = null;
+        }
+
+        return currentHead;
+    }
+
+    // unshift - adds a new node to the beginning of the list
+    // Create a new node using the value passed to the function
+    // If there is no head property on the list, set the head and tail to be the newly created node
+    // Otherwise, set the newly created node's next property to be the current head property on the list
+    // Set the head property on the list to be that newly created node
+    // Increment the length of the list by 1
+    // Return the linked list
+    unshift(val){
+        const newNode = new Node(val);
+
+        if(!this.head){
+            this.head = newNode;
+            this.tail = this.head;
+        } else {
+            newNode.next = this.head;
+            this.head = newNode;
+        }
+
+        this.length ++;
+        return this;
+    }
 }
-
-const list = new SinglyLinkedList;
-list.push(20);
-
